6a165a12e942a4c0320d0def235005d053f7b738
bismillakhans <bismillakhans@gmail.com>
2025-02-05 12:43:15 +0530

    feat :CRUD operation added

--- demo/settings.py
+++ demo/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-    "todo"
+    "todo",
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- todo/views.py
+++ todo/views.py
@@ -2,17 +2,31 @@ from django.shortcuts import render
 from django.http import JsonResponse
 # Create your views here.
 from .models import TodoItem
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view(["GET", "POST"])
 def index(request):
+    if request.method=="POST":
+        title=request.POST.get("title")
+        item=TodoItem(title=title)
+        item.save()
+        return Response({"id":item.id,"title":item.title,"completed":item.completed})
     items=TodoItem.objects.all()
     tods=[]
     for item in items:
         tods.append({"id":item.id,"title":item.title,"completed":item.completed})
-    return JsonResponse(tods,safe=False)
+    return Response(tods,safe=False)
 
 
-def create(request):
-    title=request.POST.get("title")
-    item=TodoItem(title=title)
-    item.save()
-    return JsonResponse({"message":"Item created","id":item.id})
+@api_view([ "PUT", "DELETE"])
+def update(request,id):
+    item=TodoItem.objects.get(id=id)
+    if request.method=="PUT":
+        title=request.POST.get("title")
+        item.title=title
+        item.save()
+        return Response({"id":item.id,"message":"updated"},status=200)
+    if request.method=="DELETE":
+        item.delete()
+        return Response({"message":"deleted"},status=200)
+
